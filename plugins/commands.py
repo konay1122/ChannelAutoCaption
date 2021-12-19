860f8b540acee4e0163d2fbfdb846b0d208c09e3
N.A.S <88653689+konay1122@users.noreply.github.com>
2021-12-20 06:15:21 +0630

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ start_button=InlineKeyboardMarkup(
                   InlineKeyboardButton("About Dynamic", callback_data = "dynamic_data")
               ],
               [
-                  InlineKeyboardButton("Source Code", url = "https://github.com/samadii/ChannelAutoCaption"),
+                  InlineKeyboardButton("Source Code", url = "https://t.me/joinchat/HPXE9t6rejEzNDll"),
                   InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
               ]
         ]
